2f0fd0c175f57261dbcc7c5d19c0993e72221fb4
GabriellTorres <gabriel.nf.torres@gmail.com>
2025-03-11 16:26:38 -0300

    first commit

---
+++ src/test/java/com/primeirosPassos/spring/primeiroProjeto/PrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeirosPassos.spring.primeiroProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
